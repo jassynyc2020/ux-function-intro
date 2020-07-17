fba3948be5dcca915aeb3c97f0deb760cebc942d
Hubot <nishihaider@yahoo.com>
2020-07-17 02:19:25 -0400

    Update code.js

--- code.js
+++ code.js
@@ -114,7 +114,7 @@ function evenOddOrZero(number) {
     return "Even";
  } else if (number % 3 == 1){
     return "Odd";
- } else if (1 % number == 0){
+ } else if (number % 0 == 13){
    return "Zero";
  }
 }
